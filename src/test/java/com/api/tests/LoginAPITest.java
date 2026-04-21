b9b6dd6c68df1ca1d4fa8611c18dec6a5abcb7c3
TahirSkill23 <tahirs9045@gmail.com>
2026-04-22 01:26:38 +0530

    Test Commit

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -14,7 +14,7 @@ import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class LoginAPITest {
-	//@ Tahir Mohd Asif
+	//@ Tahir
 	@Test
 	public void loginAPITest() throws IOException {
     UserCredentials userCreds=new UserCredentials("iamfd", "password");
