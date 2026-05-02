de74142cc5234bf8b5c6daaea87e61d7a542f5d4
TahirSkill23 <tahirs9045@gmail.com>
2026-05-03 01:03:17 +0530

    DateTimeUtil Class:
    Created DateTimeUtil class to provide the past days time in ISO UTC
    standard.
    We have used Instant class from java.

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -17,6 +17,9 @@ import com.api.request.model.Customer_product;
 import com.api.request.model.Problems;
 import com.api.utils.AuthTokenProvider;
 import com.api.utils.ConfigManager;
+import com.api.utils.DateTimeUtil;
+
+import static com.api.utils.DateTimeUtil.*;
 import com.api.utils.SpecUtil;
 
 import io.restassured.http.ContentType;
@@ -28,7 +31,7 @@ public class CreateJobAPITest {
 	public void createJobAPITest() throws IOException {
 		Customer customer=new Customer("Tahasf", "Mohd", "9897462699", "", "ttest123@putsbox.com", "");
 		Customer_address customerAddress=new Customer_address("207", "Aerialux", "UCO Bank Ln", "Restro", "Gachibowli", "500032", "India", "TG");
-		Customer_product customerProduct=new Customer_product("2025-07-31T18:30:00.000Z", "44453143462197", "44453143462197", "44453143462197", "2025-07-31T18:30:00.000Z", 1, 2);
+		Customer_product customerProduct=new Customer_product(DateTimeUtil.getTimeDaysAgo(10), "44463143462197", "44463143462197", "44463143462197", DateTimeUtil.getTimeDaysAgo(10), 1, 2);
 		Problems problems=new Problems(1, "Heating Issue");
 		Problems [] problemsArray=new Problems[1];
 		List<Problems> problemList=new ArrayList<Problems>();
