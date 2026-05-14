ba06a56b6a92d93f202de0d86f5acc5d731805b9
TahirSkill23 <tahirs9045@gmail.com>
2026-05-15 01:07:25 +0530

    Created fakeDataGenerator Utility for create job API test so that 100 of jobs can be created with different dynamic data.
    Also created CreateJobAPIDatadrivenTest for data driven testing using
    FakeDataGenerator utility.
    
    Created the respective data provider as well.

---
+++ src/test/java/com/api/utils/FakerDataGenerator.java
@@ -0,0 +1,104 @@
+package com.api.utils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+import com.api.request.model.CreateJobPayload;
+import com.api.request.model.Customer;
+import com.api.request.model.Customer_address;
+import com.api.request.model.Customer_product;
+import com.api.request.model.Problems;
+import com.github.javafaker.Faker;
+
+public class FakerDataGenerator {
+	private static Faker faker = new Faker(new Locale("en-IND"));
+	private final static String COUNTRY = "India";
+	private static Random random = new Random();
+	private final static int MST_SERVICE_LOCATION_ID = 0;
+	private final static int MST_PLATFORM_ID = 2;
+	private final static int MST_WARRANTY_STATUS_ID = 1;
+	private final static int MST_OEN_ID = 1;
+	private final static int PRODUCT_ID = 1;
+	private final static int MST_MODEL_ID = 1;
+
+	private FakerDataGenerator() {
+
+	}
+
+	public static CreateJobPayload generatefakeCreateJobData() {
+
+		Customer customer = generateFakeCustomerData();
+		Customer_address customerAddress = generatefakeCustomerAddressData();
+		Customer_product customerProduct = generateFakeCustomerProduct();
+		List<Problems> problemList = generateFakeProblemList();
+		CreateJobPayload payload = new CreateJobPayload(MST_SERVICE_LOCATION_ID, MST_PLATFORM_ID,
+				MST_WARRANTY_STATUS_ID, MST_OEN_ID, customer, customerAddress, customerProduct, problemList);
+		return payload;
+	}
+
+	public static Iterator<CreateJobPayload> generatefakeCreateJobData(int count) {
+		List<CreateJobPayload> payloadList = new ArrayList<CreateJobPayload>();
+		for (int i = 1; i <= count; i++) {
+			Customer customer = generateFakeCustomerData();
+			Customer_address customerAddress = generatefakeCustomerAddressData();
+			Customer_product customerProduct = generateFakeCustomerProduct();
+			List<Problems> problemList = generateFakeProblemList();
+			CreateJobPayload payload = new CreateJobPayload(MST_SERVICE_LOCATION_ID, MST_PLATFORM_ID,
+					MST_WARRANTY_STATUS_ID, MST_OEN_ID, customer, customerAddress, customerProduct, problemList);
+			payloadList.add(payload);
+		}
+		return payloadList.iterator();
+	}
+
+	private static List<Problems> generateFakeProblemList() {
+		String fakeremark = faker.lorem().sentence(5);
+		int problemId = random.nextInt(10) + 1;
+		Problems problem = new Problems(problemId, fakeremark);
+		List<Problems> problemList = new ArrayList<Problems>();
+		problemList.add(problem);
+		return problemList;
+	}
+
+	private static Customer_product generateFakeCustomerProduct() {
+		String dop = DateTimeUtil.getTimeDaysAgo(20);
+		String imeiSerialNumber = faker.numerify("###############");
+		String popurl = faker.internet().url();
+
+		Customer_product customerProduct = new Customer_product(dop, imeiSerialNumber, imeiSerialNumber,
+				imeiSerialNumber, popurl, PRODUCT_ID, MST_MODEL_ID);
+		return customerProduct;
+	}
+
+	private static Customer_address generatefakeCustomerAddressData() {
+		String flatNumber = faker.numerify("###");
+		String aprtmentName = faker.address().firstName();
+		String streetName = faker.address().streetName();
+		String landmark = faker.address().lastName();
+		String area = faker.address().lastName();
+		String pincode = faker.numerify("#####");
+
+		String state = faker.address().state();
+
+		Customer_address customerAddress = new Customer_address(flatNumber, aprtmentName, streetName, landmark, area,
+				pincode, COUNTRY, state);
+		return customerAddress;
+	}
+
+	private static Customer generateFakeCustomerData() {
+		String fName = faker.name().firstName();
+		String lName = faker.name().lastName();
+		String mobileNumber = faker.numerify("989#######");
+		String alternateMobileNumber = faker.numerify("999#######");
+		String customerEmailaddress = faker.internet().emailAddress();
+		String altCustomerEmailAddress = faker.internet().emailAddress();
+
+		Customer customer = new Customer(fName, lName, mobileNumber, alternateMobileNumber, customerEmailaddress,
+				altCustomerEmailAddress);
+
+		return customer;
+	}
+
+}
--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -13,6 +13,7 @@ import org.testng.annotations.DataProvider;
 import com.api.request.model.CreateJobPayload;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
+import com.api.utils.FakerDataGenerator;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 
@@ -35,5 +36,10 @@ public class DataProviderUtils {
 			payloadList.add(tempPayload);
 		}
 		return payloadList.iterator();
+	}
+	
+	@DataProvider(name = "CreateJobAPiFakerDataProvider", parallel=true)
+	public static Iterator<CreateJobPayload> createjobAPIFakerDataProvider() {
+		return FakerDataGenerator.generatefakeCreateJobData(5);
 	}
 }
